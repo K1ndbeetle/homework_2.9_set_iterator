b53df5212d3dd927a36049454524a433c863449e
K1ndbeetle <alx-s@bk.ru>
2023-02-14 23:59:11 +0300

    Homework is done

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -1,5 +1,7 @@
 package Driver;
 
+import java.util.Objects;
+
 public abstract class Driver {
     private String FIO;
     private boolean driverLicense;
@@ -23,4 +25,24 @@ public abstract class Driver {
 
     public abstract void refuelCar();
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Driver driver = (Driver) o;
+        return driverLicense == driver.driverLicense && experience
+                == driver.experience && Objects.equals(FIO, driver.FIO);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(FIO, driverLicense, experience);
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО : " + FIO + ", " + "наличие водительских прав - " + driverLicense + ", "
+                + "стаж - " + experience + " лет/год(ов)";
+    }
 }
